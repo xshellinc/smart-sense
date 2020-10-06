8946d84f8d551cec7278c4479a19ba8c8ec2efcd
tomotomo <tomotomobile@gmail.com>
2020-10-06 20:39:31 +0900

    fix🔧 main-alert.py

--- main-alert.py
+++ main-alert.py
@@ -5,12 +5,14 @@ from wbgt import *
 from flu import *
 
 import time
+from datetime import datetime
 
 
 IFTTT_KEY = ''
 CHECK_SPAN = 60 * 10
 
 sense = SenseHat()
+# イベント名は後で変更するので最初は何でも良い
 webhook = Webhook('demo', IFTTT_KEY)
 
 
@@ -22,36 +24,51 @@ def alert(message):
     if not res.ok:
         print('Request failed with status code', res.status_code)
 
+
 def main():
     thi_level = 0
     wbgt_level = 0
     vh_level = 0
     while True:
+        now = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
         temp = sense.get_temperature()
         hum = sense.get_humidity()
 
         payload = {
-            "value1": temp,
-            "value2": hum,
+            "value1": now,
+            "value2": temp,
+            "value3": hum,
         }
         res = webhook.post(event='send_data', payload=payload)
         if not res.ok:
             print('Request failed with status code', res.status_code)
 
+        '''
+        不快指数レベルが４以上に変化したらアラートを出す
+        '''
         thi = get_thi(temp, hum)
         level = get_thi_level(thi)
-        if thi_level < 4 and level >= 4:
-            alert('不快指数のレベルが上昇しています')
+        if (thi_level < 4 and level >= 4) or (thi_level < 5 and level >= 5):
+            alert(f'不快指数のレベルが上昇しています。現在の不快指数は{thi}です。')
+        thi_level = level
 
+        '''
+        熱中症警戒度が3以上に変化したらアラートを出す
+        '''
         wbgt = get_wbgt(temp, hum)
         level, msg = get_wbgt_level(wbgt)
-        if wbgt_level < 4 and level >=4:
-            alert(f'熱中症にご注意ください: {msg}')
+        if (wbgt_level < 3 and level >= 3) or (wbgt_level < 4 and level >= 4):
+            alert(f'【{msg}】熱中症にご注意ください！現在の暑さ指数は{wbgt}です。')
+        wbgt_level = level
 
+        '''
+        インフルエンザ警戒度が3以上に変化したらアラートを出す
+        '''
         vh = get_vh(temp, hum)
         level, msg = get_flu_level(vh)
-        if vh_level < 4 and level >=4:
-            alert(f'インフルエンザにご注意ください: {msg}')
+        if (vh_level < 3 and level >= 3) or (vh_level < 4 and level >= 4):
+            alert(f'【{msg}】インフルエンザにご注意ください。現在の絶対湿度は{vh}g/m3です。加湿しましょう。')
+        vh_level = level
 
         time.sleep(CHECK_SPAN)
 
--- thi.py
+++ thi.py
@@ -5,14 +5,14 @@ def get_thi(temp, hum):
 def get_thi_level(thi):
     if thi < 70:
         return 1
-    elif thi < 75:
+    if thi < 75:
         return 2
-    elif thi < 80:
+    if thi < 80:
         return 3
-    elif thi < 85:
+    if thi < 85:
         return 4
-    else:
-        return 5
+
+    return 5
 
 
 if __name__ == '__main__':
