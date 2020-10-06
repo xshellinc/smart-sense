7cae3ea55a4d9ddbc38ee9661c2334c4d2c45d8d
tomotomo <tomotomobile@gmail.com>
2020-10-06 19:18:00 +0900

    update payload

--- main-ifttt.py
+++ main-ifttt.py
@@ -1,10 +1,12 @@
 from sense_hat import SenseHat
+#from sense_emu import SenseHat
 from ifttt import Webhook
 
 import time
+from datetime import datetime
 
 
-IFTTT_KEY = 'cwyhruP2opLtSGJt7bO6zi'
+IFTTT_KEY = ''
 CHECK_SPAN = 60 * 10
 
 sense = SenseHat()
@@ -14,8 +16,9 @@ webhook = Webhook('send_data', IFTTT_KEY)
 def main():
     while True:
         payload = {
-            "value1": sense.get_temperature(),
-            "value2": sense.get_humidity(),
+            "value1": datetime.now().strftime("%Y/%m/%d %H:%M:%S"),
+            "value2": sense.get_temperature(),
+            "value3": sense.get_humidity(),
         }
 
         res = webhook.post(payload=payload)
