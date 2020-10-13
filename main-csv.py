6735c40e1281c80ed0c804c6670e466a201bddfa
yuta <uosotm@gmail.com>
2020-10-13 17:09:24 +0900

    Update main-csv.py

--- main-csv.py
+++ main-csv.py
@@ -1,25 +1,29 @@
 from sense_hat import SenseHat
+#from sense_emu import SenseHat
 
-from datetime import datetime
 import csv
 import time
+from datetime import datetime
 
+
+TEMP_DELTA = 0
+CHECK_SPAN = 10
 
 sense = SenseHat()
 
 
 def main():
     while True:
-        temp = sense.get_temperature()
+        now = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+        temp = sense.get_temperature() - TEMP_DELTA
         hum = sense.get_humidity()
-        created_at = datetime.now().isoformat()
-        row = [temp, hum, created_at]
-
+        row = [now, temp, hum]
+        
         with open('data.csv', 'a') as f:
             writer = csv.writer(f)
             writer.writerow(row)
-
-        time.sleep(5)
+            
+        time.sleep(CHECK_SPAN)
 
 
 if __name__ == '__main__':
